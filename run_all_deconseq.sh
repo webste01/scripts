5484dc719eb4ac8cc32757caaeca48a031249b5c
webste01 <elizabeth.webster@icahn.mssm.edu>
2016-06-10 12:04:26 -0400

    Metagenomic Analysis

---
+++ run_all_deconseq.sh
@@ -0,0 +1,10 @@
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_RC00284/RC00284_S4_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id RC00284_S4_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01244/CD01244_S6_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01244_S6_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01287/CD01287_S10_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01287_S10_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01331/CD01331_S1_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01331_S1_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01351/CD01351_S2_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01351_S2_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01382/CD01382_S3_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01382_S3_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01389/CD01389_S5_L008_R1_001.fastq  -dbs hsref -dbs_retain bact -id CD01389_S5_L008_R1_001
+perl deconseq.pl -f /sc/orga/projects/InfectiousDisease/cdiff_metagenomics/Raw/DNA.IlluminaHiSeq2500.WGS/Sample_CD01404/CD01404_S9_L008_R1_001.fastq -dbs hsref -dbs_retain bact -id CD01404_S9_L008_R1_001
+
+
