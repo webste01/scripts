009281f35b6982cd6748ec40cec08c3b948e3ec7
Elizabeth Webster <elizabeth.webster@icahn.mssm.edu>
2016-11-19 15:42:24 -0500

    Delete motif_and_mods_standalone.sh

--- motif_and_mods_standalone.sh
+++
@@ -1,4 +0,0 @@
-source /sc/orga/projects/pacbio/modules/smrtanalysis/2.2.0/install/smrtanalysis_2.3.0.140936/etc/setup.sh
-
-smrtpipe.py  --params /sc/orga/scratch/webste01/re-sequencing/simple_mot_mod.xml xml:/sc/orga/scratch/webste01/all_strains/bash5.xml -D TMP=/sc/orga/scratch/webste01/re-sequencing/temp -D SHARED_DIR=/sc/orga/scratch/webste01/re-sequencing/shared  --distribute --debug --nobascheck --output=/sc/orga/scratch/webste01/re-sequencing/all_strains_again
-
